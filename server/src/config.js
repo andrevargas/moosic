0f53f5d61ead2f7bda1ec97fc897f0b1bc529222
André Vargas <andrelv510@gmail.com>
2017-07-04 13:32:22 -0300

    Neo4j & Docker

--- server/src/config.js
+++
@@ -1,9 +0,0 @@
-const LASTFM_API_URL = 'https://ws.audioscrobbler.com/2.0';
-const LASTFM_API_KEY = 'ba143b9a2ad3c4e207b428988c59fd11';
-const LASTFM_REQUEST_URL = `${LASTFM_API_URL}?api_key=${LASTFM_API_KEY}&format=json`;
-
-module.exports = {
-    LASTFM_API_URL,
-    LASTFM_API_KEY,
-    LASTFM_REQUEST_URL
-};
--- server/src/server.js
+++ server/src/server.js
@@ -1,62 +1,15 @@
-'use strict';
-
-const Express = require('express');
-const Realm = require('realm');
+const express = require('express');
+const neo4j = require('neo4j');
 const bodyParser = require('body-parser');
 
-const schemas = require('./schemas');
-const RBC = require('./rbc');
-const { loadStartupData } = require('./bootstrap');
+const app = express();
+const db = new neo4j.GraphDatabase({
+    url: 'http://moosic_db_neo4j:7474',
+    auth: { username: 'neo4j', password: '123321' }
+});
 
-const app = Express();
-const realm = new Realm({ schema: schemas });
-
-let user = null;
 app.use(bodyParser.json());
-loadStartupData(realm);
 
-app.post('/user', (req, res) => {
-    realm.write(() => {
-        user = realm.create('User', req.body)
-    });
-    res.send().status(201);
-});
+app.get('/', (req, res) => res.send('hello world!'));
 
-app.put('/user', (req, res) => {
-    user = realm.objects('User')
-        .find(item => item.id === req.body.userId);
-    res.send().status(200);
-});
-
-app.get('/album', (req, res) => {
-    const { offset = 0, limit = 10 } = req.query;
-    const albums = realm.objects('Album')
-        .map(item => item)
-        .splice(offset, limit);
-    res.contentType('json')
-        .send(JSON.stringify(albums))
-        .status(200);
-});
-
-app.get('/recommended', (req, res) => {
-    const albums = user.clickedAlbums;
-    if (albums.length < 0) {
-        return res.send(JSON.stringify([])).status(200);
-    }
-    albums.forEach(item => {
-        const database = realm.objects('Album')
-            .map(item => ({
-                name: item.name,
-                artist: item.artist,
-                playCount: item.playCount
-            }));
-        const mappedAlbum = {
-            name: item.name,
-            artist: item.artist,
-            playCount: item.playCount
-        };
-        RBC.testCase(database, mappedAlbum);
-    });
-});
-
-app.listen(8080, () => console.log('server running'));
+app.listen(process.env.NODE_PORT, () => console.log('server running'));
